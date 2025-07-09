1ad9c1bf33e7dea3a27bac9dd9a923aa4d411649
Jaikesh Kain <164343591+Jaikeshkain@users.noreply.github.com>
2025-07-09 23:00:18 +0530

    Update app.ts

--- backend/app.ts
+++ backend/app.ts
@@ -22,7 +22,7 @@ const app = express();
 const server=http.createServer(app)
 export const io = new Server(server,{
   cors:{
-    origin:process.env.FRONTEND_URL,
+    origin:"https://collab-board-u698.onrender.com/",
     methods:["GET","POST","PUT","DELETE"]
   }
 })
@@ -32,7 +32,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(morgan('dev'));
 app.use(cors({
-  origin: process.env.FRONTEND_URL,
+  origin: "https://collab-board-u698.onrender.com/",
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true
 }));
